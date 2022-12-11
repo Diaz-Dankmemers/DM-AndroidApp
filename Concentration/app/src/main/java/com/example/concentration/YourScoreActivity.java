528045ce24d58804a509f7329d0da7b98e64b7c6
Daniel Menkir <danielmenkir9@gmail.com>
2022-12-10 21:09:12 -0800

    created landscape view for screens that need it.

--- Concentration/app/src/main/java/com/example/concentration/YourScoreActivity.java
+++ Concentration/app/src/main/java/com/example/concentration/YourScoreActivity.java
@@ -3,6 +3,7 @@ package com.example.concentration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class YourScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_score);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
 
         backToMainMenu = findViewById(R.id.mMainMenu);
         backToMainMenu.setOnClickListener(new View.OnClickListener() {
